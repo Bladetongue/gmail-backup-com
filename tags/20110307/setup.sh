7244cdd59a21c1789403d83ad95d17e917a072ba
honza.svec@gmail.com <honza.svec@gmail.com@82206d49-b8b3-ae45-5068-82854b000a41>
2011-03-07 20:22:13 +0000

    New Gmail Backup release 20110307

---
+++ tags/20110307/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#python2.5 -O /usr/lib/python2.5/py_compile.py gmail-backup-gui.py
+#python2.5 -O /usr/lib/python2.5/py_compile.py gmail-backup.py
+#python2.5 -O /usr/lib/python2.5/py_compile.py gmb.py
+mkdir dist_SH
+cp gmail-backup.py gmail-backup-gui.py gmb.py dist_SH
+cp gmb.gif gmb.ico dist_SH
+cp gmail-backup.pot dist_SH
+mkdir -p dist_SH/messages/cs_CZ/LC_MESSAGES
+cp messages/cs_CZ/LC_MESSAGES/gmail-backup.mo dist_SH/messages/cs_CZ/LC_MESSAGES/gmail-backup.mo
+cp messages/cs_CZ.po dist_SH/messages/cs_CZ.po
+cp -r svc dist_SH
+rm -rf dist_SH/svc/.svn
+rm -rf dist_SH/svc/scripting/.svn
